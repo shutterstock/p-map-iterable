58a69e36bedf9d09bb31a4995e5ac6142168a19b
Harold Hunt <hhunt@giphy.com>
2025-02-12 23:58:30 -0500

    Update IterableQueueMapperSimple example

--- examples/iterable-queue-mapper-simple.ts
+++ examples/iterable-queue-mapper-simple.ts
@@ -31,7 +31,7 @@ async function main() {
   let callCount = 0;
 
   // Create an item processor with IterableQueueMapperSimple
-  const prefetcher = new IterableQueueMapperSimple(
+  const backgroundFlusher = new IterableQueueMapperSimple(
     // mapper function
     async (value: number): Promise<void> => {
       const myCallCount = callCount++;
@@ -39,9 +39,7 @@ async function main() {
 
       console.log(`Mapper Call Start ${myCallCount}, Value: ${value}, Total: ${total}`);
 
-      // Simulate fetching an async item with varied delays
-      // await sleep(10 * (callCount % 7));
-      // Wait short random time
+      // Simulate flushing an async item with varied delays
       await sleep(Math.random() * 10000);
 
       if (value % 5 === 0) {
@@ -53,26 +51,27 @@ async function main() {
     { concurrency: 3 },
   );
 
-  // Add items to the queue in the background
+  // Add items to be flushed to the queue in the background
+  // This will pause when the queue is full and resume when there is capacity
   const jobAdder = (async () => {
     for await (const item of iterator) {
       console.log(`Enqueue Start ${item}`);
-      await prefetcher.enqueue(item);
+      await backgroundFlusher.enqueue(item);
       console.log(`Enqueue Done  ${item}`);
     }
   })();
 
   // Wait for the job adder to finish adding the jobs
-  // (it's throughput is constrained by the prefetcher's concurrency)
+  // (it's throughput is constrained by the flushers's concurrency)
   await jobAdder;
 
-  // Wait for the prefetcher to finish processing all items
-  await prefetcher.onIdle();
+  // Wait for the async flusher to finish flushing all items
+  await backgroundFlusher.onIdle();
 
   // Check for errors
-  if (prefetcher.errors.length > 0) {
+  if (backgroundFlusher.errors.length > 0) {
     console.error('Errors:');
-    prefetcher.errors.forEach(({ error, item }) =>
+    backgroundFlusher.errors.forEach(({ error, item }) =>
       console.error(
         `${item} had error: ${(error as Error).message ? (error as Error).message : error}`,
       ),
@@ -80,6 +79,7 @@ async function main() {
   }
 
   console.log(`Total: ${total}`);
+  console.log('Note - It is intended that there are errors in this example');
 }
 
 void main();
